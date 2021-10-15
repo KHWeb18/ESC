0019e6e91a774b4b07eb7e84bcd8ee6d6a502b33
ekank <ekankr2@naver.com>
2021-10-15 21:45:32 +0900

    로그인 세션 유지, 로그아웃 (VUEX)

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,8 +1,32 @@
 import {
+    COOKIE_TO_SESSION, REMOVE_IS_LOGIN, REMOVE_SESSION, SET_IS_LOGIN,
 
 } from './mutation-types'
 
-
 export default {
-
+    // 쿠키값을 state.session에 저장함
+    cookieToSession ({ commit }) {
+        let data
+        if(this.$cookies.get("user") !== null){
+            data = this.$cookies.get("user")
+        }else {
+            data = null
+        }
+        commit(COOKIE_TO_SESSION, data)
+    },
+    // 로그인 유무 확인
+    setIsLogin ({ commit, state }) {
+        let temp
+        if(state.session !== null) {
+            temp = true
+        }else{
+            temp = false
+        }
+        commit(SET_IS_LOGIN, temp)
+    },
+    logout ({ commit }) {
+        this.$cookies.remove('user')
+        commit(REMOVE_SESSION, null)
+        commit(REMOVE_IS_LOGIN, false)
+    },
 }
