a7f5de80fcca0d8a793aea9ee13ceba7a2d261ce
yoo-jong-hyeon <gowert612@gmail.com>
2021-10-19 20:26:54 +0900

    [유종현]메인페이지 주차장, 충전소, 고객센터 카드

--- frontend/src/views/MainHomePage.vue
+++ frontend/src/views/MainHomePage.vue
@@ -1,26 +1,194 @@
 <template>
+	<v-app>
 		<header class="intro">
-		<div class="intro-body">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8 col-md-offset-2">
-						<p class="intro-text">전기차 시대!
-							<br>전기차를 준비하는 당신에게
-							<br>필요한 모든 정보와 꿀팁! <span class="name">EVS</span></p>
-						<div class="mouse-warp">
-							<a href="#mouse-here" class="page-scroll">
-								<div class="mouse">
-									<div class="scroll"></div>
-								</div>
-							</a>
+			<div class="intro-body">
+				<v-container>
+					<v-row>
+						<div class="col-md-8 col-md-offset-2">
+							<p class="intro-text">전기차 시대!
+								<br>전기차를 준비하는 당신에게
+								<br>필요한 모든 정보와 꿀팁!<span class="name">EVS</span></p>
+							<div class="mouse-warp">
+								<a href="#mouse-here" class="page-scroll">
+									<div class="mouse">
+										<div class="scroll"></div>
+									</div>
+								</a>
+							</div>
 						</div>
+					</v-row>
+				</v-container>
+			</div>
+		</header>
+<br>
+
+<div id="mouse-here"></div>
+
+	<!-- welcome section 시작-->
+	<section id="welcome">
+		<v-container>
+				<h3>Welcome To EVS</h3>
+					<div class="subText">
+						<p>전기차 그 이상의 정보를 제공합니다.
+							<br>EVS는 우리가 알아야 할 전기차 시대의 정보, 새로운 인프라를 모두 전달하고자 합니다.</p>
 					</div>
-				</div>
+		</v-container>
+	</section>
+<!-- welcome section 끝 -->
+<section id="what-ev">
+	<v-container>
+		<div style="display: inline-block;">
+			<div style="float: left">
+				<h1>전기자동차</h1>
+					<p style=
+						"font-size: 20px;
+						color: #616161;
+						font-size: 20px;
+						margin-top: 10px;
+						line-height: 1.7;"
+					>
+					전기자동차의 작동원리 및 구조에 대한 내용과 충전시 주의사항에 관한 내용,<br>
+					전기자동차의 관리 방법 및 운행 요령에 대한 자세한 내용을 확인 하실 수 있습니다.</p>
+				<v-card-actions>
+					<v-btn
+						color="blue"
+						outlined
+						rounded
+						href="#"
+					>
+					전기자동차 원리 및 구조 >
+					</v-btn>
+				</v-card-actions>	
+			</div>
+			<div style="float: right">
+				<img 
+				src="@/assets/img/EV-Car.png">
 			</div>
 		</div>
-	</header>
+	</v-container>
+</section>
+<!--주차장, 충전소, 고객센터 카드 시작 -->
+<section id="par-char-center">
+	<v-container>
+		<v-layout>
+			<v-flex>
+				<v-row >
+					<v-col class="white-space">
+						<!-- 주차장 찾기 카드 시작 -->
+						<v-card class="mx-auto" width="400" href="#" > 
+							<div class="card-logo">
+								<v-img
+									max-height="250"
+									max-width="100"
+									height="100px"
+									src="@/assets/img/Parking.png"
+								>
+								</v-img>
+							</div>
+								
+							<v-card-title>
+								<v-icon large left color="grey"> mdi-map-marker-outline</v-icon>
+								주차장 찾기
+							</v-card-title>
+							<v-card-subtitle>
+							Parking
+							</v-card-subtitle>
+							<v-card-text class="text--primary">
+								<div>더이상 헤매지 말자!</div>
+								<div>주변 주차장을 찾아보자</div>
+							</v-card-text>
+							<v-card-actions>
+								<v-btn
+									color="blue"
+									outlined
+									rounded
+								>
+								서비스 바로가기
+								</v-btn>
+							</v-card-actions>
+						</v-card>
+					</v-col>
+			<!-- 주차장 찾기 카드 끝 -->
+
+			<!-- 충전소 찾기 카드 시작 -->
+					<v-col class="white-space"> 
+						<v-card class="mx-auto" width="400" href="#">
+							<div class="card-logo">
+								<v-img
+									max-height="150"
+									max-width="100"
+									height="100px"
+									src="@/assets/img/EV-Station.png"
+								>
+								</v-img>
+							</div>
+							<v-card-title>
+								<v-icon large left color="grey"> mdi-map-marker-outline</v-icon>
+								충전소 찾기
+							</v-card-title>
+							<v-card-subtitle>
+							EV-Charging St
+							</v-card-subtitle>
+							<v-card-text class="text--primary">
+								<div>주변 충전소 정보</div>
+								<div>바로 확인하세요.</div>
+							</v-card-text>
+							<v-card-actions>
+								<v-btn
+									color="blue"
+									outlined
+									rounded
+								>
+								서비스 바로가기
+								</v-btn>
+							</v-card-actions>
+						</v-card>
+					</v-col>
+					<!-- 충전소 찾기 카드 끝 -->
+					<!-- 고객센터 카드 시작 -->
+					<v-col class="white-space">
+						<v-card class="mx-auto" width="400" href="#"> 
+							<div class="card-logo">
+								<v-img
+									max-height="250"
+									max-width="100"
+									height="100px"
+									src="@/assets/img/Service_Center.png"
+								>
+								</v-img>
+							</div>
+							<v-card-title>
+								<v-icon large left color="grey"> mdi-help</v-icon>
+								고객센터
+							</v-card-title>
+							<v-card-subtitle>
+							Service Center
+							</v-card-subtitle>
+							<v-card-text class="text--primary">
+								<div>어떤 서비스를</div>
+								<div>도와드릴까요?</div>
+							</v-card-text>
+							<v-card-actions>
+								<v-btn
+									color="blue"
+									outlined
+									rounded
+								>
+								서비스 바로가기
+								</v-btn>
+							</v-card-actions>
+						</v-card>
+						<!-- 고객센터 카드 끝 -->
+					</v-col>
+				</v-row>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</section>
+<!--주차장, 충전소, 고객센터 카드 끝 -->
 
 
+	</v-app>
 </template>
 
 <script>
@@ -33,7 +201,62 @@ export default {
 
 
 <style scoped>
+body{
+	padding: 0%;
+	margin: 0%
+}
+#what-ev{
+	padding-top: 200px;
+	padding-bottom: 200px;
+	display: grid;
+	place-content: center;
+}
 
+.white-space{
+	padding-top: 10px;
+	padding-bottom: 10px;
+}	
+#par-char-center{
+	padding-top: 50px;
+	padding-bottom: 50px;
+	display: grid;
+	place-content: center;
+	}
+h1{
+	font-family: 'Montserrat', sans-serif;
+	font-weight: 700;
+}
+h3 {
+	color: black;
+	font-weight: 700;
+	font-size: 70px;
+	font-family: 'Montserrat', sans-serif;
+	text-align: center;
+}
+
+h3:after {
+	content: '';
+	width: 200px;
+	height: 3px;
+	background: skyblue;
+	display: block;
+	margin: 15px auto;
+	transition: all .2s ease-in-out;
+}
+
+h3:hover:after {
+	width: 570px;
+}
+
+.subText{
+	text-align: center;
+	color: #616161;
+	font-size: 20px;
+	margin-top: 10px;
+	line-height: 1.7;
+}
+
+.card-logo{float: right; padding: 50px 50px;}
 
 .intro {
 	display: table;
@@ -41,15 +264,21 @@ export default {
 	height: auto;
 	text-align: center;
 	color: white;
-	background: url(https://org1-www.hyundai.com/content/dam/hyundai/kr/ko/images/brand/brandstory/eco/eco-fuel-cell-electric-vehicle-keyvisual-w.jpg) no-repeat top center scroll;
+	background: url(https://org1-www.hyundai.com/content/dam/hyundai/kr/ko/images/brand/brandstory/eco/eco-fuel-cell-electric-vehicle-keyvisual-w.jpg) no-repeat center scroll;
 	background-size: cover;
+	
+}
+.back{
+	display: table;
+	width: 100%;
+	height: 100%;
 	
 }
 
 .intro .intro-body {
 	display: table-cell;
 	vertical-align: middle;
-	padding: 15% 0;
+	padding: 17.2% 0;
 	background: rgba(0, 0, 0, 0.5);
 }
 
