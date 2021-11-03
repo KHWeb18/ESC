9e7f42da58826d856388c449786d13380c59ed98
yoo-jong-hyeon <gowert612@gmail.com>
2021-11-03 14:59:54 +0900

    [유종현] 마이페이지(IsLikePage 추가)

---
+++ frontend/src/views/myPage/IsLikePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <my-page-menu />
+  </div>
+</template>
+
+<script>
+import MyPageMenu from "../../components/myPage/MyPageMenu.vue";
+
+export default {
+  name: "IsLikePage",
+  components: { MyPageMenu },
+};
+</script>
