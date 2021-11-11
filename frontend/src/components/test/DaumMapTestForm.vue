e2d1f5a0b4b39afdd808d20268885ae9662c1573
ekank <ekankr2@naver.com>
2021-11-11 20:33:51 +0900

    [임익환] 맵 오버레이 제작 1

--- frontend/src/components/test/DaumMapTestForm.vue
+++ frontend/src/components/test/DaumMapTestForm.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="kmap" ref="map">
+
     </div>
 </template>
 
@@ -36,9 +37,25 @@ export default {
 
       // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
       var content = `<div class="overlay_info">
-      <a href="https://place.map.kakao.com/17600274" target="_blank"><strong>충전소 정보</strong></a>
+      <div class="topTitle"><span>충전소 정보</span></div>
       <div class="desc">
-      <span class="address">구의축구공원 ~~~</span>
+      <span class="name">종묘 공영주차장</span>
+      <hr class="solid">
+
+      <div class="address">
+      <i class="fas fa-map-marker-alt"></i>
+      <span>서울특별시 종로구 종로 157 가나다라마ㅏ사</span>
+      </div>
+
+      <div class="tel">
+      <i style="color: #3F51B5; font-size: 20px" class="fas fa-phone"></i>
+      <span>1661-9408</span>
+      </div>
+
+      <div class="status">
+      <i style="color: #3F51B5; font-size: 20px" class="fas fa-info-circle"></i>
+      <span>사용 가능</span>
+      </div>
       </div>
       </div>`;
 
@@ -51,11 +68,14 @@ export default {
         position: position,
         content: content,
         xAnchor: 0.53, // 커스텀 오버레이의 x축 위치입니다. 1에 가까울수록 왼쪽에 위치합니다. 기본값은 0.5 입니다
-        yAnchor: 1.4 // 커스텀 오버레이의 y축 위치입니다. 1에 가까울수록 위쪽에 위치합니다. 기본값은 0.5 입니다
+        yAnchor: 1.3, // 커스텀 오버레이의 y축 위치입니다. 1에 가까울수록 위쪽에 위치합니다. 기본값은 0.5 입니다
+        removable: true
       });
 
-      customOverlay.setMap(mapInstance)
-
+      // customOverlay.setMap(mapInstance)
+      kakao.maps.event.addListener(marker, 'click', function() {
+        customOverlay.setMap(mapInstance);
+      });
 
     },
   methods: {
@@ -86,9 +106,10 @@ export default {
 }
 
 .overlay_info:nth-of-type(n) {
-  border:0; box-shadow: 0px 1px 2px #888;
+  border:0;
+  box-shadow: 0px 1px 2px #888;
 }
-.overlay_info a
+.overlay_info .topTitle
 {
   display: block;
   background: #d95050 url(https://user-images.githubusercontent.com/83811729/141263590-aba1330b-7993-4eb3-b2f3-f8f83f0eaea1.png) no-repeat right 14px center;
@@ -98,28 +119,68 @@ export default {
   font-size: 10px;
   border-radius: 6px 6px 0 0
 }
-.overlay_info a strong
+.overlay_info .topTitle span
 {
-  margin-left: 10px;
+  margin-left: 40px;
   color: #ffffff;
-  font-size: 2em;
+  font-size: 2.1em;
+  font-weight: bold;
 }
 .overlay_info .desc
 {
-  padding:50px;
+  font-family: "Montserrat", sans-serif;
+  padding: 20px 0 10px 0;
+  text-align: left;
+  margin: -10px 20px 0 20px;
   position: relative;
   min-width: 190px;
-  height: 56px
+  white-space: normal;
 }
-.overlay_info .address
+.overlay_info .name
 {
-  font-size: 12px;
+  font-size: 1.2em;
+  font-weight: bold;
   color: #333;
-  position: absolute;
-  left: 20px;
-  right: 14px;
-  top: 14px;
-  white-space: normal
+}
+hr.solid {
+  border-top: 2px solid #bbb;
+}
+
+.overlay_info .address span{
+  font-size: 13px;
+  color: #333;
+  margin-top: 4px;
+}
+.overlay_info .address .fa-map-marker-alt{
+  margin: 6px 2px 0 0;
+  color: #3F51B5;
+  font-size: 22px
+}
+.overlay_info .tel span{
+  font-weight: 600;
+  font-size: 1em;
+  color: #333;
+  margin-top: 8px;
+}
+.overlay_info .tel .fa-phone{
+  margin: 6px 2px 0 0;
+  color: #3F51B5;
+  font-size: 22px
+}
+.overlay_info .status span{
+  font-weight: 600;
+  font-size: 1em;
+  color: #333;
+  margin-top: 7px;
+}
+.overlay_info .status .fa-info-circle{
+  margin: 6px 2px 0 0;
+  color: #3F51B5;
+  font-size: 22px
+}
+
+hr.soft {
+  border-top: 0.7px solid #aaa;
 }
 .overlay_info:after
 {
