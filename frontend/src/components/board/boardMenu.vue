4d85d6a809dbb17e6d33fe7073d6457aa46afdff
bombom23 <tkddn5678@naver.com>
2021-11-18 01:18:04 +0900

    [한상우]2021 11/18 게시판 꾸미기

---
+++ frontend/src/components/board/boardMenu.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <v-card  dark elevation="0" style="position:absolute; margin-top: 13px; margin-left: 250px; width:180px">
+    <v-card-title > 
+     
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-title > 
+     <v-btn width="130" outlined route :to="{ name: 'BoardListPage'}"> 전체글({{AllBoardLength}})</v-btn>
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-title > 
+     <v-btn width="130" outlined route :to="{ name: 'FreeBoardListPage'}"> 자유게시판</v-btn>
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-title > 
+     <v-btn width="130" outlined route :to="{ name: 'ChargingBoardListPage'}"> 충전소게시판</v-btn>
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-title > 
+     <v-btn width="130" outlined route :to="{ name: 'ParkingBoardListPage'}"> 주차장게시판</v-btn>
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-title > 
+     <v-btn width="130" outlined route :to="{ name: 'SuggestionBoardListPage'}"> 건의게시판</v-btn>
+    </v-card-title>
+    <v-card-title > 
+     <v-btn width="130" outlined route :to="{ name: 'NoticeListPage'}"> 공지사항</v-btn>
+    </v-card-title>
+    <v-divider></v-divider>
+  </v-card>
+</div>
+</template>
+
+<script>
+import { mapActions ,mapState } from 'vuex'
+export default {
+    data(){
+        return{
+            AllBoardLength:Number(this.boardList.length)
+        }
+    },
+computed: {
+    ...mapState(['boardList'])
+},
+methods:{
+    ...mapActions(['fetchBoardList'])
+},
+created(){
+    this.fetchBoardList()
+}
+
+
+}
+</script>
