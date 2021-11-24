db378f56a17b95725f53449ae653394424f47b3b
ekank <ekankr2@naver.com>
2021-11-24 21:26:11 +0900

    [임익환] 원본 링크 추가

--- frontend/src/views/ApiTestPage2.vue
+++ frontend/src/views/ApiTestPage2.vue
@@ -3,6 +3,7 @@
     <div v-for="(item,idx) in news" :key="idx">
       <div>설명 : {{item.description}}</div>
       <div>링크 : {{item.link}}</div>
+      <div>원본링크 : {{item.originallink}}</div>
       <div>날짜 : {{item.pubDate}}</div>
       <div>제목 : {{item.title}}</div>
       <v-divider></v-divider>
